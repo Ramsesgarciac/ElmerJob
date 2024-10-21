d9249583fc6672a4cd1798ec1cb58b9555ded22e
Ramsesgarciac <garciacuevasramses101@gmail.com>
2024-10-21 06:46:43 -0600

    pokemamadas

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark">
           <main className="flex min-h-screen flex-col items-center p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-              <Link href="/"><h2 className="text-2xl text-bold">PokemonFinder</h2></Link>
+              <Link href="/"><h2 className="text-2xl text-bold">Pokedexxx</h2></Link>
             </div>
             {children}
           </main>
--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -30,9 +30,9 @@ export function PokemonGrid({ pokemonList } : PokemonGridProps) {
     return (
         <>
         <div>
-            <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
+            <h3 className="text-2xl py-6 text-center">Busca Tu Pokmenon Favorito</h3>
             <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="pokemonName">Pokemon Name</Label>
+                <Label htmlFor="pokemonName">Pokemon:</Label>
                 <Input 
                     type="text" 
                     value={searchText} 
@@ -42,7 +42,7 @@ export function PokemonGrid({ pokemonList } : PokemonGridProps) {
                     onChange={(e) => setSearchText(e.target.value)}
                 />
             </div>
-            <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
+            <h3 className="text-3xl pt-12 pb-6 text-center">Tu coleccion Pokemon</h3>
         </div>
 
         <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
